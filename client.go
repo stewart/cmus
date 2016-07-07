7174d6a5eae7112b3ada29ddea04cd60dd271ff2
Andrew Stewart <andrew@stwrt.ca>
2016-07-06 22:20:14 -0700

    Add Client methods to write to / read from socket

--- client.go
+++ client.go
@@ -1,6 +1,8 @@
 package cmus
 
 import (
+	"fmt"
+	"io"
 	"net"
 )
 
@@ -31,6 +33,47 @@ func (c *Client) Connect() error {
 	c.conn = conn
 
 	return nil
+}
+
+// sends a command to cmus
+func (c *Client) write(str string) {
+	fmt.Fprintf(c.conn, str+"\n")
+}
+
+// reads a response from cmus
+func (c *Client) read() (string, error) {
+	var data string
+
+	buf := make([]byte, 8192)
+	newline := false
+	len := 0
+
+	for {
+		rc, err := c.conn.Read(buf)
+		if err != nil && err != io.EOF {
+			return data, err
+		}
+
+		len += rc
+
+		if newline && buf[0] == '\n' {
+			break
+		}
+
+		if len == 1 && buf[0] == '\n' {
+			break
+		}
+
+		if rc > 1 && buf[rc-1] == '\n' && buf[rc-2] == '\n' {
+			data += string(buf[:rc-1])
+			break
+		}
+
+		newline = buf[rc-1] == '\n'
+		data += string(buf)
+	}
+
+	return data, nil
 }
 
 func (c *Client) Status() {
