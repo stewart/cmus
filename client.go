50fa22c63997a961a1b0f90f5bcf29261bcbee70
Andrew Stewart <andrew@stwrt.ca>
2016-07-07 18:58:51 -0700

    Improve implementation of Client.read
    Use bufio.Scanner, rather than manually grabbing chunks from the
    connection.

--- client.go
+++ client.go
@@ -1,9 +1,10 @@
 package cmus
 
 import (
+	"bufio"
 	"fmt"
-	"io"
 	"net"
+	"strings"
 )
 
 type Client struct {
@@ -41,42 +42,24 @@ func (c *Client) write(str string) {
 }
 
 // reads a response from cmus
-func (c *Client) read() (string, error) {
-	var data string
+func (c *Client) read() string {
+	lines := []string{}
 
-	buf := make([]byte, 8192)
-	newline := false
-	len := 0
+	scanner := bufio.NewScanner(c.conn)
 
-	for {
-		rc, err := c.conn.Read(buf)
-		if err != nil && err != io.EOF {
-			return data, err
-		}
-
-		len += rc
-
-		if newline && buf[0] == '\n' {
+	for scanner.Scan() {
+		text := scanner.Text()
+		if text == "" {
 			break
 		}
 
-		if len == 1 && buf[0] == '\n' {
-			break
-		}
-
-		if rc > 1 && buf[rc-1] == '\n' && buf[rc-2] == '\n' {
-			data += string(buf[:rc-1])
-			break
-		}
-
-		newline = buf[rc-1] == '\n'
-		data += string(buf)
+		lines = append(lines, text)
 	}
 
-	return data, nil
+	return strings.Join(lines, "\n")
 }
 
-func (c *Client) Status() (string, error) {
+func (c *Client) Status() string {
 	c.write("status")
 	return c.read()
 }
