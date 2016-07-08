4e0b4d4c7a5f2de9c5d0869dc1a993520e9b1d52
Andrew Stewart <andrew@stwrt.ca>
2016-07-07 22:36:28 -0700

    Improve godocs

--- client.go
+++ client.go
@@ -8,15 +8,14 @@ import (
 	"sync"
 )
 
+// Client is a connection to a cmus server.
 type Client struct {
 	conn net.Conn
 	mut  sync.Mutex
 }
 
-func NewClient() *Client {
-	return &Client{}
-}
-
+// Connects the Client to cmus. The path socket is automatically determined,
+// based on the same logic cmus uses internally.
 func (c *Client) Connect() error {
 	path, err := socketPath()
 	if err != nil {
@@ -38,7 +37,8 @@ func (c *Client) Connect() error {
 	return nil
 }
 
-// sends a command to cmus
+// Sends a command (provided as a string) to cmus. Will return an error if the
+// client is not connected to cmus, or if an error occured while writing.
 func (c *Client) write(str string) error {
 	if c.conn == nil {
 		return errors.New("client is not connected")
@@ -52,7 +52,8 @@ func (c *Client) write(str string) error {
 	return nil
 }
 
-// reads a response from cmus
+// Reads a response from cmus. Will block until one is received. Returns an
+// error if one occured during response fetching.
 func (c *Client) read() (string, error) {
 	lines := []string{}
 
@@ -70,7 +71,7 @@ func (c *Client) read() (string, error) {
 	return strings.Join(lines, "\n"), scanner.Err()
 }
 
-// executes a command against cmus, and returns the result
+// Cmd runs a command against cmus, and returns the result of that command.
 func (c *Client) Cmd(command string) (string, error) {
 	c.mut.Lock()
 	defer c.mut.Unlock()
@@ -82,43 +83,80 @@ func (c *Client) Cmd(command string) (string, error) {
 	return c.read()
 }
 
+// Status is a shorthand for Cmd("status"). It returns status information from
+// cmus, including metadata about the currently playing track and various
+// settings.
 func (c *Client) Status() (string, error) {
 	return c.Cmd("status")
 }
 
+// Play is a shorthand for Cmd("player-play " + filename). It plays the given
+// track, or, if none is specified, [re]plays the current track from the
+// beginning.
 func (c *Client) Play(filename string) (string, error) {
 	return c.Cmd("player-play " + filename)
 }
 
-// Toggles play/pause
+// PlayPause is a shorthand for Cmd("player-pause"). It toggles pause.
 func (c *Client) PlayPause() (string, error) {
 	return c.Cmd("player-pause")
 }
 
+// Stop is a shorthand for Cmd("player-stop"). It stops playback.
 func (c *Client) Stop() (string, error) {
 	return c.Cmd("player-stop")
 }
 
+// Prev is a shorthand for Cmd("player-prev"). It skips to the previous track.
 func (c *Client) Prev() (string, error) {
 	return c.Cmd("player-prev")
 }
 
+// Next is a shorthand for Cmd("player-prev"). It skips to the next track.
 func (c *Client) Next() (string, error) {
 	return c.Cmd("player-prev")
 }
 
+// Seek is a shorthand for Cmd("seek " + time). It seeks to an absolute or
+// relative position. Position is given in the format "[+-](<num>mh |
+// [HH:]MM:SS)". Some examples:
+//
+// Seek 1 minute backward
+//     client.Seek("-1m")
+//
+// Seek 5 seconds forward
+//     client.Seek("+5")
+//
+// Seek to absolute position 1h
+//     client.Seek("1h")
+//
+// Seek 90 seconds forward
+//     client.Seek("+1:30")
 func (c *Client) Seek(time string) (string, error) {
 	return c.Cmd("seek " + time)
 }
 
+// Volume is a shorthand for Cmd("vol " + level). It sets, increases, or
+// decreases volume.
+//
+// To increase or decrease volume prefix the value with - or +, otherwise value
+// is treated as absolute volume.
+//
+// Both absolute and relative values can be given as percentage units (suffixed
+// with %) or as internal values (hardware may have volume in range 0-31 for
+// example).
 func (c *Client) Volume(level string) (string, error) {
 	return c.Cmd("vol " + level)
 }
 
+// Shuffle is a shorthand for Cmd("toggle shuffle"). It toggles whether or not
+// the playback order is shuffled.
 func (c *Client) Shuffle() (string, error) {
 	return c.Cmd("toggle shuffle")
 }
 
+// Repeat is a shorthand for Cmd("toggle repeat"). It toggles whether or not
+// playback will repeat after all tracks are played.
 func (c *Client) Repeat() (string, error) {
 	return c.Cmd("toggle repeat")
 }
--- cmus.go
+++ cmus.go
@@ -1,2 +1,2 @@
-// package cmus implements an interface to control cmus through a socket
+// Package cmus implements an interface to control cmus through a unix socket
 package cmus
